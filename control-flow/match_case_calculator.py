5eb2729fb9743baf23b66148e2272a9e7e563b0e
BasemGhoste <basemreiyad@gmail.com>
2024-09-22 21:22:43 +0300

    Add files via upload

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -5,23 +5,21 @@ num2 = float(input("Enter the secund number: "))
 # Requesting a calculation from the user
 operation = input("Choose the operation (+, -, *, /): ")
 
-
 # Match case for obvious reason
-
 match operation:
     case '+':
         result = num1 + num2
-        print(f"The result is {result}.")
+        print(f"The result is: {result}.")
     case '-':
         result = num1 - num2
-        print(f"The result is {result}.")
+        print(f"The result is: {result}.")
     case '*':
         result = num1 * num2
-        print(f"The result is {result}.")
+        print(f"The result is: {result}.")
     case '/':
         if num2 != 0:
             result = num1 / num2
-            print(f"The result is {result}.")
+            print(f"The result is: {result}.")
         else:
             print("Cannot divide by zero.")
     case _:
