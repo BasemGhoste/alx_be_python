a153a6c71b58cd5a6f3883fc28a4c3c9c6fd967a
BasemGhoste <https://github.com/BasemGhoste/alx_be_python.git>
2024-10-01 13:08:20 +0300

    gg

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,7 +1,5 @@
-# Global Conversion Factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
-
 
 def convert_to_celsius(fahrenheit):
     """Converts Fahrenheit to Celsius using the global conversion factor."""
