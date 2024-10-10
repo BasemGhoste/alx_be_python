0c9a2d9b04d799617b6567199b2f1a246135bb69
BasemGhoste <https://github.com/BasemGhoste/alx_be_python.git>
2024-10-10 19:07:57 +0300

    gg

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -9,8 +9,9 @@ def safe_divide(numerator, denominator):
         
         # محاولة إجراء القسمة
         result = num / denom
-        return f"The result of the division is {result:.2f}"
+        return f"The result of the division is {result:.1f}"  # تغيير هنا
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:
         return "Error: Please enter numeric values only."
+
